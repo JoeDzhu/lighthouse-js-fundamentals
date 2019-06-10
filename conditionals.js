4260a7e46aefc7015fe3819cc8be43653055efc1
JoeJoe <JJ@JJ.JJ>
2019-06-10 13:16:35 -0400

    add more exmaples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,16 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const raining = !true;
+const cold = !false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
