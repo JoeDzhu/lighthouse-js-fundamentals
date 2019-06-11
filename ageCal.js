a46e1187eb57286cef28daee2287da4fc0a511dc
JoeJoe <JJ@JJ.JJ>
2019-06-11 17:12:46 -0400

    age calculator.

--- ageCal.js
+++ ageCal.js
@@ -1,6 +1,6 @@
 const ageCalculator = function (name, yearOfBirth, currentYear) {
-  return name + " is " + (currentYear - yearOfBirth) + " old.";
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
 };
 
-console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Miranda", 1989, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
